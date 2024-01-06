535f8a624b0b137d0784ca35305342d87c9031ee
aufaramdhn <aufa.233040028@mail.unpas.ac.id>
2024-01-06 10:24:05 +0700

    update all

--- assets/js/spreadSheet.js
+++ assets/js/spreadSheet.js
@@ -4,30 +4,25 @@ const form = document.forms["submit-to-google-sheet"];
 const btnSend = document.querySelector(".send");
 const loader = document.querySelector(".loaders");
 const alertSuccess = document.querySelector(".alert-success");
-const alertDanger = document.querySelector(".alert-danger");
-
-function closeAlert(event) {
-  let AlertClose = event.target;
-  AlertClose.closest(".alert-close").remove();
-}
+const alertDanger = document.querySelector(".alert-warning");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  loader.classList.toggle("none");
-  btnSend.classList.toggle("none");
+  loader.classList.toggle("d-none");
+  btnSend.classList.toggle("d-none");
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
-      btnSend.classList.toggle("none");
-      loader.classList.toggle("none");
-      alertSuccess.classList.toggle("alert");
       form.reset();
+      btnSend.classList.toggle("d-none");
+      loader.classList.toggle("d-none");
+      alertSuccess.classList.toggle("d-none");
       console.log("Success!", response);
     })
     .catch((error) => {
-      btnSend.classList.toggle("none");
-      loader.classList.toggle("none");
-      alertDanger.classList.toggle("alert");
       form.reset();
+      btnSend.classList.toggle("d-none");
+      loader.classList.toggle("d-none");
+      alertSuccess.classList.toggle("d-none");
       console.error("Error!", error.message);
     });
 });
